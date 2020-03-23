c9b22fea8de82f7024ee1f60723448d26ac974b9
Nandanaknair12 <nandanaknair1224@gmail.com>
2020-03-23 10:56:35 +0530

    update

--- app/src/main/java/com/example/ekathapro/Updatedeletepresi.java
+++ app/src/main/java/com/example/ekathapro/Updatedeletepresi.java
@@ -4,12 +4,14 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -20,7 +22,7 @@ import com.google.firebase.database.ValueEventListener;
 public class Updatedeletepresi extends AppCompatActivity {
     EditText uni,plc,mob,user;
     Button btnSer,btnDel,btnUpd;
-    DatabaseReference reference;
+    DatabaseReference reference,reference1;
     Spinner wa,no;
     Unitpres unitpres;
     String war,untno;
@@ -46,7 +48,7 @@ public class Updatedeletepresi extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 war=wa.getSelectedItem().toString().trim();
-             untno=no.getSelectedItem().toString().trim();
+                untno=no.getSelectedItem().toString().trim();
 
                 reference= FirebaseDatabase.getInstance().getReference().child(war);
                 reference.addValueEventListener(new ValueEventListener() {
@@ -83,19 +85,76 @@ public class Updatedeletepresi extends AppCompatActivity {
                 mob1=mob.getText().toString().trim();
                 user1=user.getText().toString().trim();
 
-                reference= FirebaseDatabase.getInstance().getReference().child(war).child(untno);
-                Query query=reference.orderByChild("status").equalTo(true);
+                if (unitna1.equals(""))
+                {
+                    uni.setError("cannot be empty");
+                }
+                else if (unitpla1.equals(""))
+                {
+                    plc.setError("cannot be empty");
+                }
+                else if (mob1.equals(""))
+                {
+                    mob.setError("cannot be empty");
+                }
+                else if (user1.equals(""))
+                {
+                    user.setError("cannot be empty");
+                }
+                else
+                {
+                    reference1= FirebaseDatabase.getInstance().getReference().child(war).child(untno);
+                    Query query=reference1.orderByChild("status").equalTo("true");
+                    query.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                            dataSnapshot.getRef().child("una").setValue(unitna1);
+                            dataSnapshot.getRef().child("upl").setValue(unitpla1);
+                            dataSnapshot.getRef().child("umo").setValue(mob1);
+                            dataSnapshot.getRef().child("uus").setValue(user1).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Toast.makeText(Updatedeletepresi.this, "Updated", Toast.LENGTH_SHORT).show();
+
+                                    uni.setText("");
+                                    plc.setText("");
+                                    mob.setText("");
+                                    user.setText("");
+                                }
+                            });
+
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                        }
+                    });
+                }
+            }
+        } );
+
+        btnDel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                war=wa.getSelectedItem().toString().trim();
+                untno=no.getSelectedItem().toString().trim();
+
+                reference1= FirebaseDatabase.getInstance().getReference().child(war).child(untno);
+                Query query=reference1.orderByChild("uno").equalTo("untno");
                 query.addListenerForSingleValueEvent(new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
 
-                        for(DataSnapshot snapshot:dataSnapshot.getChildren())
-                        {
-                            snapshot.getRef().child("una").setValue(unitna1);
-                            snapshot.getRef().child("upl").setValue(unitpla1);
-                            snapshot.getRef().child("umo").setValue(mob1);
-                            snapshot.getRef().child("uus").setValue(user1);
-                        }
+                        dataSnapshot.getRef().removeValue();
+                        Toast.makeText(getApplicationContext(), "deleted", Toast.LENGTH_SHORT).show();
+
+                        uni.setText("");
+                        plc.setText("");
+                        mob.setText("");
+                        user.setText("");
                     }
 
                     @Override
@@ -103,8 +162,7 @@ public class Updatedeletepresi extends AppCompatActivity {
 
                     }
                 });
-
             }
-        } );
+        });
     }
 }
